e23a04ab5b19fbd3f27a0c57f9d9d72556486071
JOY <piczo617@gmail.com>
2023-02-23 16:23:50 +0900

    Create 05 콘테스트 총점 순위.sql

---
+++ SQL/중급/서브쿼리/05 콘테스트 총점 순위.sql
@@ -0,0 +1,25 @@
+SELECT
+    hackers.hacker_id,
+    hackers.name,
+    SUM(max_score) AS total_score
+FROM
+    hackers
+JOIN
+    (SELECT 
+        hacker_id,
+        challenge_id,
+        MAX(score) AS max_score
+    FROM
+        Submissions
+    GROUP BY
+        hacker_id,
+        challenge_id
+      ) AS max_scores ON max_scores.hacker_id = hackers.hacker_id
+GROUP BY
+    hackers.hacker_id,
+    hackers.name
+HAVING
+    SUM(max_score) != 0
+ORDER BY
+    total_score DESC,
+    hacker_id;
